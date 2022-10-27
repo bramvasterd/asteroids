a912fa59c3bc854e7a5b0da03bde7e3bff44bdd8
Max van der Donk <m.j.vanderdonk@students.uu.nl>
2022-10-27 15:53:06 +0200

    model

--- src/Model.hs
+++ src/Model.hs
@@ -2,17 +2,33 @@
 --   which represent the state of the game
 module Model where
 
-data InfoToShow = ShowNothing
-                | ShowANumber Int
-                | ShowAChar   Char
+type Angle    = Float
+type Velocity = Float
+type Score    = Float
+type Position = (Float, Float)
+
+data Asteroid   = Asteroid {position:: Position, size :: Float}
+data Player     = Player {position:: Position, shooting:: Bool, playerAngle:: Angle, lives::Int}
+data Bullet     = Bullet {velocity:: Velocity, angle:: Angle}
+data Rocket     = Rocket {position:: Position}
+
+data GameStatus = Initial | Playing | Paused | Stopped
+
+
+
+
 
 nO_SECS_BETWEEN_CYCLES :: Float
 nO_SECS_BETWEEN_CYCLES = 5
 
 data GameState = GameState {
-                   infoToShow  :: InfoToShow
-                 , elapsedTime :: Float
-                 }
+                            player :: Player
+                            bullets :: [Bullet]
+                            asteroids :: [Asteroid]
+                            rockets :: [Rockets]
+                            state :: GameStatus 
+                            seed :: Int
+                            elapsedTime :: Float}
 
 initialState :: GameState
 initialState = GameState ShowNothing 0
